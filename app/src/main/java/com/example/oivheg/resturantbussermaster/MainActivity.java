2959c465a9b8c153469e826500f3c2f3973e42b6
oivheg <oivheg@gmail.com>
2017-02-13 08:30:46 +0100

    Added Server Client Code, as weel as added Dynamic buttons at runtime

--- app/src/main/java/com/example/oivheg/resturantbussermaster/MainActivity.java
+++ app/src/main/java/com/example/oivheg/resturantbussermaster/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.oivheg.resturantbussermaster;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-TextView msg ;
+    private static final int NUM_ROWS = 0;
+    private static final int NUM_COL = 0;
+    Server server;
+    TextView infoip, msg;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        infoip = (TextView) findViewById(R.id.infoip);
+        msg = (TextView) findViewById(R.id.msg);
+        server = new Server(this);
+        infoip.setText(server.getIpAddress() + ":" + server.getPort());
+        populateClients();
     }
+
+    private void populateClients() {
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COL; col++) {
+
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        server.onDestroy();
+    }
+
 }
