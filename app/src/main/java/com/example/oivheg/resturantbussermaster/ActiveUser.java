d8011c5de11327d936afb4340d21b8b419fc5fe1
oivheg <oivheg@gmail.com>
2017-02-13 11:19:46 +0100

    images added to button, list of users also works

---
+++ app/src/main/java/com/example/oivheg/resturantbussermaster/ActiveUser.java
@@ -0,0 +1,12 @@
+package com.example.oivheg.resturantbussermaster;
+
+
+/**
+ * Created by oivhe on 13.02.2017.
+ */
+
+public class ActiveUser {
+    private String name;
+
+
+}
--- app/src/main/java/com/example/oivheg/resturantbussermaster/MainActivity.java
+++ app/src/main/java/com/example/oivheg/resturantbussermaster/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.oivheg.resturantbussermaster;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,11 +11,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int NUM_ROWS = 2;
-    private static final int NUM_COL = 2;
+    private static final int NUM_ROWS = 1;
+    private static int NUM_COL = 0;
     Server server;
     TextView infoip, msg;
-    String activeUsers[] = {"øivind", "Jan", "Espen"};
+    String activeUsers[] = {"øivind", "Espen", "Linda", "kåre"};
 
 
     @Override
@@ -25,28 +26,44 @@ public class MainActivity extends AppCompatActivity {
         msg = (TextView) findViewById(R.id.msg);
         server = new Server(this);
         infoip.setText(server.getIpAddress() + ":" + server.getPort());
+
+        findClients();
         populateClients();
+    }
+
+    private void findClients() {
+        for (int users = 0; users < activeUsers.length; users++) {
+            NUM_COL++;
+        }
     }
 
     private void populateClients() {
         TableLayout table = (TableLayout) findViewById(R.id.tableForClients);
         for (int row = 0; row < NUM_ROWS; row++) {
+
             TableRow tableRow = new TableRow(this);
+            tableRow.setBackgroundColor(Color.BLACK);
             tableRow.setLayoutParams(new TableLayout.LayoutParams(
                     TableLayout.LayoutParams.MATCH_PARENT,
                     TableLayout.LayoutParams.MATCH_PARENT,
                     1.0f
+
             ));
+
             table.addView(tableRow);
             for (int col = 0; col < NUM_COL; col++) {
                 final String FINAL_USER_NAME = activeUsers[col];
                 Button button = new Button(this);
 
+
                 button.setLayoutParams(new TableRow.LayoutParams(
                         TableRow.LayoutParams.MATCH_PARENT,
                         TableRow.LayoutParams.MATCH_PARENT,
                         1.0f));
-                button.setText(FINAL_USER_NAME + row + "" + col);
+
+                button.setBackgroundColor(Color.BLACK);
+                button.setBackgroundResource(R.drawable.waiter_no);
+                button.setText(FINAL_USER_NAME + " " + row + " " + col);
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
